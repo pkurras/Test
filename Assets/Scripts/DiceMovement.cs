fa863b30d67fc240dd8da269d845dd2ac5d98c60
Patrick <VisualBasic@hotmail.de>
2020-12-02 13:53:20 +0100

    Dice game fix
    [*] dice game working

---
+++ Assets/Scripts/DiceMovement.cs
@@ -0,0 +1,39 @@
+﻿
+using UnityEngine;
+
+public class DiceMovement : MonoBehaviour
+{
+    public float speedMultiplier = 1.0f;
+    public Vector3 DiceOffset;
+    private Rigidbody OwnRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OwnRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float TimeMul = Time.deltaTime * 200.0f;
+        transform.rotation = transform.rotation * Quaternion.Euler(new Vector3(Random.Range(0, TimeMul), Random.Range(0, TimeMul), Random.Range(0, TimeMul)));
+
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(mouseRay, out hit, 1 << 8)) 
+        {
+            Vector3 destination = hit.point + DiceOffset;
+            OwnRigidbody.velocity = (destination - transform.position)*speedMultiplier;
+        }
+        else
+        {
+            var rigidbody = GetComponent<Rigidbody>();
+            rigidbody.velocity = Vector3.zero;
+        }
+    }
+    public void Reset()
+    {
+        transform.position = Vector3.zero + Vector3.up * 5.0f;
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,66 +5,83 @@ using UnityEngine.SocialPlatforms.Impl;
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> Dices;
+    public List<int> ScoreList;
     public bool GameReady = false;
-    public int Score = 0;
     public float DiceFacingBias = 0.99f;
 
     void Update()
     {
-        Restart();
+        if (Input.GetMouseButtonDown(0))
+        {
+            foreach (var Dice in Dices)
+                Dice.GetComponent<DiceMovement>().enabled = false;
+            GameReady = true;
+        }
 
         bool AllDicesResting = true;
-
         foreach (GameObject Dice in Dices)
         {
             if (!Dice.GetComponent<Rigidbody>().IsSleeping())
                 AllDicesResting = false;
         }
 
-        GameReady = AllDicesResting;
-
-        if (AllDicesResting)
+        if (AllDicesResting && GameReady)
         {
-            // TODO: Show Score
-            foreach (GameObject Dice in Dices)
-            {
-                if (Vector3.Dot(Dice.transform.forward, Vector3.up) >= DiceFacingBias)
-                {
-                    Score += 1;
-                }
-                else if (Vector3.Dot(-Dice.transform.forward, Vector3.up) >= DiceFacingBias)
-                {
-                    Score += 6;
-                }
-                else if (Vector3.Dot(Dice.transform.right, Vector3.up) >= DiceFacingBias)
-                {
-                    Score += 4;
-                }
-                else if (Vector3.Dot(-Dice.transform.right, Vector3.up) >= DiceFacingBias)
-                {
-                    Score += 3;
-                }
-                else if (Vector3.Dot(Dice.transform.up, Vector3.up) >= DiceFacingBias)
-                {
-                    Score += 5;
-                }
-                else if (Vector3.Dot(-Dice.transform.up, Vector3.up) >= DiceFacingBias)
-                {
-                    Score += 2;
-                }
-                else
-                {
-                    Debug.Log("Facing no side!");
-                    Restart();
-                    return;
-                }
-            }
+            CalculateScore();
+            GameReady = false;
         }
     }
-    void Restart()
+    void CalculateScore()
     {
-        GameReady = false;
-        Score = 0;
+        int Score = 0;
+        foreach (GameObject Dice in Dices)
+        {
+            if (Vector3.Dot(Dice.transform.forward, Vector3.up) >= DiceFacingBias)
+            {
+                Score += 1;
+            }
+            else if (Vector3.Dot(-Dice.transform.forward, Vector3.up) >= DiceFacingBias)
+            {
+                Score += 6;
+            }
+            else if (Vector3.Dot(Dice.transform.right, Vector3.up) >= DiceFacingBias)
+            {
+                Score += 4;
+            }
+            else if (Vector3.Dot(-Dice.transform.right, Vector3.up) >= DiceFacingBias)
+            {
+                Score += 3;
+            }
+            else if (Vector3.Dot(Dice.transform.up, Vector3.up) >= DiceFacingBias)
+            {
+                Score += 5;
+            }
+            else if (Vector3.Dot(-Dice.transform.up, Vector3.up) >= DiceFacingBias)
+            {
+                Score += 2;
+            }
+            else
+            {
+                Debug.Log("Facing no side!");
+                RestartRound();
+                return;
+            }
+        }
+        ScoreList.Add(Score);
+        RestartRound();
+    }
+    public void RestartRound()
+    {
+        foreach (var Dice in Dices)
+        {
+            Dice.GetComponent<DiceMovement>().enabled = true;
+            Dice.GetComponent<DiceMovement>().Reset();
+        }
+    }
+    public void RestartGame()
+    {
+        ScoreList.Clear();
+        RestartRound();
     }
 
     public void ExitGame()
